1912bfdb7f014b265220f1feebf64b40d564580c
Erin Hao <yunhaocs@hotmail.com>
2014-10-25 00:26:53 -0600

    week3.final

--- Week3/week3.R
+++ Week3/week3.R
@@ -1,127 +1,89 @@
-train <- read.csv("~/Google Drive/Bike Share/train.csv", stringsAsFactors=FALSE)
-test <- read.csv("~/Google Drive/Bike Share/test.csv", stringsAsFactors=FALSE)
-### Set variable type ###
+source("Week3/helper.R")
 
+train <- preprocess(read.csv("train.csv", stringsAsFactors=FALSE))
+test <-  preprocess(read.csv("test.csv", stringsAsFactors=FALSE))
 
-preprocess <- function(data){
-  data$season = factor(data$season,levels = c(1,2,3,4),ordered = FALSE)
-  data$holiday = factor(data$holiday,levels = c(0,1),ordered = FALSE)
-  data$workingday = factor(data$workingday,levels = c(0,1),ordered = FALSE)
-  data$weather = factor(data$weather,levels = c(4,3,2,1),ordered = TRUE)
-  data$datetime = as.POSIXct(data$datetime, tz = "EST", format = "%Y-%m-%d %H:%M:%S")
-  holiday.level = c("Not_holiday", "holiday")
-  workingday.level = c("Not_workingday", "workingday")
-  season.level = c("winter", "spring", "summer","fall")
-  weather.level = c("heavy rain","rain","mist", "clear")
-  
-  ### Compute New Variable -> hour ###
-  data$hour =  as.numeric(strftime(data$datetime, format="%H"))
-  data$hour = factor(data$hour,ordered = TRUE)
-  data$month = as.numeric(strftime(data$datetime, format="%m"))
-  data$month = factor(data$month,ordered = TRUE)
-  month = as.numeric(strftime(data$datetime, format="%m"))
-  #month = factor(data$month,ordered = TRUE)
-  data$season[month %in% c(12,1,2)] = 1
-  data$season[month %in% c(3,4,5)] = 2
-  data$season[month %in% c(6,7,8)] = 3
-  data$season[month %in% c(9,10,11)] = 4
+#Change datetime to hour number since the start of the year
+train$datetime = unclass(train$datetime)/3600 
+train$datetime = train$datetime - train$datetime[1]
+test$datetime = unclass(test$datetime)/3600 
+test$datetime = test$datetime - test$datetime[1]
 
-  ### Compute New Variable -> weekday ###
-  data$weekday = factor(weekdays(data$datetime))
-  ### Standardize the numeric variable ###
-  scale(data$temp, center = TRUE, scale = FALSE)
-  scale(data$atemp, center = TRUE, scale = FALSE)
-  scale(data$humidity, center = TRUE, scale = FALSE)
-  scale(data$windspeed, center = TRUE, scale = FALSE)
-  return(data)
-}
-rmlse_evaluation <- function(benchmark, predict_value, target_value){
-  predict_value[predict_value< benchmark] = benchmark
-  rmsle = ((1/length(predict_value))*sum((log(predict_value+1)-log(target_value+1))^2))^0.5
-  return(rmsle)
-}
-print_output <-function(testdata,predict_value,filename){
-  output <- data.frame(datetime=testdata[,1], count=predict_value)
-  write.csv(output, file=filename, quote=FALSE, row.names=FALSE)
-}
-set_benchmark <-function(data,benchmark){
-  data[data < benchmark] = benchmark
-  return(data)
-}
-
-### Preprocess the value ###
-train = preprocess(train)
-test = preprocess(test)
-
-interaction_plot <- function(x_factor,trace_factor,data,trace_label,x_label){
-  
-  png(file=paste("Week3//image//",x_label,"_",trace_label,".png", sep = ""),
-      width=1000, 
-      height=400,
-      )
-  layout(matrix(c(1,2,3),1,3,byrow=FALSE))
-  interaction.plot(x_factor,trace_factor,data$count,col=c(1,3,2,4,5:10),lt = 1,trace.label =trace_label,fixed = TRUE,xlab = x_label,main="count",legend = FALSE)
-  legend("topleft",title = trace_label, legend = levels(trace_factor),lty = 1,col=c(1,3,2,4,5:10),cex = 1.5,bty = "n",pt.lwd = 0.5)
-  interaction.plot(x_factor,trace_factor,data$registered,col=c(1,3,2,4,5:10),lt = 1,trace.label =trace_label,fixed = TRUE,xlab = x_label,main="registered",legend = FALSE)
-  interaction.plot(x_factor,trace_factor,data$casual,col=c(1,3,2,4,5:10),lt = 1,trace.label =trace_label,fixed = TRUE,xlab = x_label,main="casual",legend = FALSE) 
-  dev.off()
-}
-
-numeric_list = list(train$temp,"temp",train$atemp,"atemp",train$humidity,"humidity",train$windspeed,"windspeed",train$hour,"hour")
-dim(numeric_list) = c(2,length(numeric_list)/2)
-factor_list = list(train$season,"season",c("winter","spring","summer","fall"),train$holiday,"holiday",c("Not_holiday", "holiday"),train$workingday,"workingday",c("Not_workingday", "workingday"),train$weather,"weather",c("heavy rain","rain","mist", "clear"))
-dim(factor_list) = c(3,length(factor_list)/3)
-
-for(m in 1:length(factor_list)/3){
-  trace.plot = factor(factor_list[[1,m]],labels=factor_list[[3,m]],ordered=TRUE)
-for (n in 1:length(numeric_list)/2){
-  interaction_plot(numeric_list[[1,n]], trace.plot,train,factor_list[[2,m]],numeric_list[[2,n]])
-}
-for(k in m+1:length(factor_list)/3){
-  interaction_plot(factor_list[[1,k]], trace.plot,train,factor_list[[2,m]],factor_list[[2,k]])
-}
-}
-
-season.plot = factor(train$season,labels=c("winter","spring","summer","fall"),ordered=TRUE)
-interaction_plot(train$atemp,season.plot,train,"season","atemp")
-workingday.plot  = factor(train$workingday,labels=c("Not_workingday", "workingday"),ordered=TRUE)
-interaction_plot(train$temp,workingday.plot,train,"workingday","temp")
-interaction_plot(train$atemp,workingday.plot,train,"workingday","atemp")
-
-interaction.plot(train$atemp,factor(train$season,labels=c("winter","spring","summer","fall"),ordered=TRUE),train$count,col=c(1,3,2,4),lt = 1,trace.label = "season",fixed = TRUE,legend = FALSE)
-legend("topleft",title = "season", legend = c("winter","spring","summer","fall"),lty = 1,col=c(1,3,2,4),cex = 0.9,bty = "n")
-interaction.plot(train$temp,factor(train$season,labels=c("winter","spring","summer","fall"),ordered=TRUE),train$count,col=c(1,3,2,4),lt = 1,trace.label = "season",fixed = TRUE)
-interaction.plot(train$windspeed,factor(train$season,labels=c("winter","spring","summer","fall"),ordered=TRUE),train$count,col=c(1,3,2,4),lt = 1,trace.label = "season",fixed = TRUE)
-interaction.plot(train$humidity,factor(train$season,labels=c("winter","spring","summer","fall"),ordered=TRUE),train$count,col=c(1,3,2,4),lt = 1,trace.label = "season",fixed = TRUE)
-
-interaction.plot(train$humidity,factor(train$workingday,labels=c(wo),ordered=TRUE),train$count,col=c(1,3,2,4),lt = 1,trace.label = "season",fixed = TRUE)
-
-
-
-
-# Exacting Features and Target
+# Exacting Features and Target #
 count.feature = train[,-c(10,11)]
 count.target = train[,12]
 
-### Linear Regression of COUNT ####
-train.lm <- lm(count ~ ., data=count.feature)
+## Pairwise lm ##
+train.lm <- lm(count ~ .*., data=count.feature)
 summary(train.lm)
-a = rmlse_evaluation(benchmark = 10, predict_value = predict(train.lm,count.feature ), target = count.target)
-test.predict.lm = predict(train.lm, test)
-test.predict.lm[test.predict.lm<10] = 10
-print_output(test,test.predict.lm,filename="lm_result.csv")
+anova(train.lm)
+train.rmsle = rmlse_evaluation(benchmark = 10, predict_value = predict(train.lm,count.feature ), target = count.target)
 
-### glm ###
+test.predict.lm = predict(train.lm, test)
+print_output(test,set_benchmark(test.predict.lm,10),filename="lm_result.csv")
+cross_validation(count.feature, count.target,5,formula(train.lm))
+## 3-variable lm ##
+train.lm <- lm(count ~ .^3, data=count.feature)
+## Remove temp
+temp.feature = count.feature[,-c(6)]
+temp.lm <- lm(count ~ .*., data=temp.feature)
+summary(temp.lm)
+cross_validation(temp.feature, count.target,5,formula(temp.lm))
+test.predict.lm = predict(temp.lm, test)
+print_output(test,set_benchmark(test.predict.lm,10),filename="lm_result.csv")
+
+
+af = formula(train.lm)
+## All subsets size > 5 ##
+
+u = vector("list",4)
+w = vector("list",4)
+for(i in 2:5){
+  list = combn(c(1:9,11:12),i)
+  best_rmsle = 100
+  best_r = 0
+  cb_rmsle = c(1,1)
+  cb_r = c(1,1)
+  n = ncol(list)
+  for(j in 1:n){
+    count.feature = train[,-c(10,11)]
+    count.target = train[,12]
+    temp.feature = count.feature[,c(list[,j],10)]
+    temp.lm = lm(count ~ .*., data=temp.feature)
+    cv.result = cross_validation(temp.feature, count.target,5,formula(temp.lm))
+    if(best_r<cv.result$r_squared){
+      best_r = cv.result$r_squared
+      cb_r = list[,j]
+    }
+    if(best_rmsle>cv.result$rmsle){
+      best_rmsle = cv.result$rmsle
+      cb_rmsle = list[,j]
+    }
+  }
+  u[[i - 1]] = cb_r
+  w[[i - 1]] = cb_rmsle
+}
+count.feature = train[,-c(10,11)]
+count.target = train[,12]
+temp.feature = count.feature[,c(w[[1]],10)]
+
+temp.lm = lm(count ~ .*., data=temp.feature)
+cv.result = cross_validation(temp.feature, count.target,5,formula(temp.lm))
+
+u6_11 = u
+w6_11 = w
+
+
+### Possion ###
 train.glm <- glm(count ~ 1+., data=count.feature,family = poisson(link = "log"))
 summary(train.glm)
 predict_value = 2^predict(train.glm,count.feature )
 a = rmlse_evaluation(benchmark = 10, predict_value = 2^predict(train.glm,count.feature ), target = count.target)
 test.predict.glm = 2^predict(train.glm, test)
 test.predict.glm[test.predict.glm<10] = 10
-print_output(test,test.predict.glm,filename="glm_result.csv")
+print_output(test,test.predict.glm,filename="possion_result.csv")
 
-#Proportion of casual rides
-prop_casual = train$casual/train$count
+
 
 ### Combination ###
 #### Exact Feature and Target ####
@@ -136,17 +98,16 @@ train.predict.casual.lm = predict(train.casual.lm,casual.feature);
 rmlse.casual = rmlse_evaluation(benchmark = 2,predict_value = train.predict.casual.lm, target = casual.target)
 test.predict.casual.lm = predict(train.casual.lm,test)
 test.predict.casual.lm = set_benchmark(data = test.predict.casual.lm,benchmark = 2)
-
+cv.casual = cross_validation(x=casual.feature,y = casual.target,formula_lm = formula(train.casual.lm))
 ### Linear regression registered count ####
 train.lm.registered= lm(registered ~.*., data = registered.feature)
 train.predict.registered.lm = predict(train.lm.registered,registered.feature)
 rmlse.registered = rmlse_evaluation(benchmark = 8, predict_value = train.predict.registered.lm , target = registered.target)
 test.predict.registered.lm = predict(train.lm.registered, test)
 test.predict.registered.lm = set_benchmark(data = test.predict.registered.lm,benchmark = 8)
+cv.registered = cross_validation(x=registered.feature,y = registered.target,formula_lm = formula(train.lm.registered))
 
-ratio = 10*train$registered/train$count
-train.glm <- glm(ratio ~ .*., data=count.feature,family = poisson())
-ratio.predict = predict(train.glm,count.feature)
+
 ### Combine count = casual + registered ###
 train.predict.count.lm = train.predict.registered.lm + train.predict.casual.lm
 rmlse.combine = rmlse_evaluation(benchmark = 10,predict_value = train.predict.count.lm, target = count.target)
@@ -154,4 +115,4 @@ test.predict.count.combine = test.predict.casual.lm + test.predict.registered.lm
 test.predict.count.combine = set_benchmark(data = test.predict.count.combine,benchmark = 10)
 print_output(test,test.predict.count.combine,filename="combine_result.csv")
 train.predict.combine = predict(train.casual.lm,casual.feature) + predict(train.lm.registered,registered.feature)
-r_squared = 1 - sum((count.target-train.predict.combine)^2)/sum((count.target-mean(count.target))^2)
+cv.registered = cross_validation(x=casual.feature,y = casual.target,formula_lm = formula(train.lm))
