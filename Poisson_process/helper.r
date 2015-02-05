7c604f67ffdd90d01df565e90bdc4a263b0ae5ed
Erin Hao <yunhaocs@hotmail.com>
2015-02-05 14:49:53 -0700

    after commit

--- Poisson_process/helper.r
+++ Poisson_process/helper.r
@@ -6,10 +6,29 @@ station.lon <- function(id){
   return(stations[stations$id == id,]$longitude)
 }
 
+
+station.id_to_index <- function(id){
+  return(which(stations==id)[1])
+}
 station.name <- function(id){
   return(stations[stations$id == id,]$name)
 }
 
 ride.start_terminal<- function(id){
   return(ride[ride$Start.terminal == id,])
+}
+
+time_to_hour <- function(time){
+  # Return num of minutes after 00:00 at the current time
+  # Args:
+  #   time(string): time in the format of "%m/%d/%Y %H:%M" or "%Y-%m-%d %H:%M:%S"
+  # Return:
+  #   number of minutes range(0-1440)
+  temp = as.POSIXlt(strptime(time,"%m/%d/%Y %H:%M"))
+  if(is.na(temp) == FALSE){
+    return(temp[[3]])
+  }else{
+    temp = as.POSIXlt(strptime(time,"%Y-%m-%d %H:%M:%S"))
+    return(temp[[3]])
+  }
 }
--- Poisson_process/post1.r
+++ Poisson_process/post1.r
@@ -10,7 +10,6 @@ qqplot(ride$Duration)
 library(MASS)
 duration_no_outlier = ride$Duration[which(ride$Duration <= 2500)]
 
-
 #### get distribution of S ####
 freqs_in = aggregate(ride$Start.terminal, by=list(ride$Start.terminal),FUN=length)
 summary(freqs_in$x)
@@ -22,16 +21,15 @@ plot(x = as.list(sapply(freqs_in$Group.1, function(x)station.lat(x))),freqs_in$x
 
 
 freqs_in[ order(-freqs_in[,2]), ][c(1:10),]
-
+freqs_in[nrow(freqs_in)/4*3,]
 top2 = c(31623,31200)
 station.name(31623)
 station.name(31200)
 id = 31623
 library(ggplot2)
 
-par(mfcol = c(2,1))
-a= station.get_hour_distribution(31623)
-station.get_hour_distribution<- function(id){
+a = station.get_hour_distribution(32045)
+station.get_hour_distribution <- function(id){
 temp.ride = ride.start_terminal(id)
 temp.hour = format(strptime(temp.ride$Start.date,"%m/%d/%Y %H:%M"),"%H")
 freq_hour = aggregate(temp.hour, by=list(temp.hour),FUN=length)
@@ -46,3 +44,36 @@ hist(temp.ride$Duration,breaks = 100)
 temp.end_count = aggregate(temp.ride$End.terminal, by=list(temp.ride$End.terminal),FUN=length)
 temp.end_count[ order(-temp.end_count[,2]), ]
 station.name(31258)
+
+##### get time matrix ######
+start_by_stations = vector(mode="list",nrow(stations))
+end_by_stations = vector(mode="list",nrow(stations)) 
+for(i in c(1:nrow(stations))){
+  start_by_stations[[i]] = vector(mode = "numeric",24)
+  end_by_stations[[i]] = vector(mode = "numeric",24)
+}
+
+for(i in c(1:nrow(ride))){
+  temp = ride[i,]
+  if(length(station.id_to_index(temp[[4]]))!=0){
+    start_by_stations[[station.id_to_index(temp[[4]])]][time_to_hour(temp[[2]])+1] = start_by_stations[[station.id_to_index(temp[[4]])]][time_to_hour(temp[[2]])+1] + 1
+  }
+  if(length(station.id_to_index(temp[[7]]))!=0){
+    end_by_stations[[station.id_to_index(temp[[7]])]][time_to_hour(temp[[5]])+1] = end_by_stations[[station.id_to_index(temp[[7]])]][time_to_hour(temp[[5]])+1] + 1
+  }
+}
+
+
+freqs_out = aggregate(ride$End.terminal, by=list(ride$End.terminal),FUN=length)
+
+summary(freqs_out$x)
+boxplot(freqs_out$x,main = "# of rides that ends at each station")
+hist(freqs_out$x,breaks = 100,xlim=c(0,7000),xlab = "# of rides that ends at station", main = "# of rides that end at each station(w/o outliers)")
+
+par(mfrow=c(1,2))
+dev.off()
+plot(x = as.list(sapply(freqs_in$Group.1, function(x)station.lon(x))),freqs_out$x,cex = 0.1,xlab = "longitude",ylab = "# of rides",main = "# of rides vs longitude")
+plot(x = as.list(sapply(freqs_in$Group.1, function(x)station.lat(x))),freqs_out$x,cex = 0.1,xlab = "latitude",ylab = "# of rides",main = "# of rides vs latitude")
+
+
+
